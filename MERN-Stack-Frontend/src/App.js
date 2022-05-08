ec0d65b3c3806142a5d50a7dce1bbe807dbd4d10
ImashaWickramasinghe <78404724+Imashawickramasinghe@users.noreply.github.com>
2022-05-08 19:58:39 +0530

    Update App.js
    Updated routes

--- MERN-Stack-Frontend/src/App.js
+++ MERN-Stack-Frontend/src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import { Route, Routes , Navigate} from "react-router-dom";
 
-
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
 import Home from "./components/Home";
 import AddFood from "./components/AddFood";
 import Foods from "./components/Food/Foods";
 import Cusfoods from "./components/Food/Cusfoods";
 import About from "./components/About";
 import FoodDetail from "./components/Food/FoodDetail";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import EditUser from "./pages/editUser/EditUser";
+import UserDetails from "./pages/UserDetails/UserDetails";
 function App() {
   
   const user = localStorage.getItem("token");
@@ -25,7 +31,12 @@ function App() {
         <Routes>
         
 			    />
-		
+          <Route exact path="/"><Home /></Route>
+          <Route path="/users"><UserList /></Route>
+          <Route path="/user/:userId"><User /></Route>
+          <Route path="/newUser"><NewUser /></Route>
+          <Route path="/update/:id" component={EditUser}></Route>
+          <Route path="/get/:id" component={UserDetails} />
           <Route path="/home" element={<Home />} exact />
           <Route path="/add" element={<AddFood />} exact />
           <Route path="/foods" element={<Foods />} exact />
